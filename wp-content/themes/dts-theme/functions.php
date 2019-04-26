70c5909da1950f586f5f1d7528bfb275fa1cd855
Tomas Hartl <lachtani@gmail.com>
2019-04-26 06:37:58 -0400

    add post-id-based html IDs to blog page listings

--- wp-content/themes/dts-theme/functions.php
+++ wp-content/themes/dts-theme/functions.php
@@ -18,7 +18,7 @@ include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
 
 // Set Localization (do not remove).
 add_action( 'after_setup_theme', 'pw_base_localization_setup' );
-function pw_base_localization_setup(){
+function pw_base_localization_setup() {
 	load_child_theme_textdomain( 'pw-base', get_stylesheet_directory() . '/languages' );
 }
 
@@ -54,7 +54,7 @@ define( 'CHILD_THEME_VERSION', '1.0.0' );
 add_action( 'wp_enqueue_scripts', 'pw_base_enqueue_scripts_styles' );
 function pw_base_enqueue_scripts_styles() {
 
-	
+
 	wp_enqueue_style( 'pw-base-fonts', '//fonts.googleapis.com/css?family=Nunito:300,400,700|Comfortaa:400,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
@@ -92,7 +92,14 @@ function pw_base_responsive_menu_settings() {
 add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
 
 // Add Accessibility support.
-add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+add_theme_support( 'genesis-accessibility', array(
+	'404-page',
+	'drop-down-menu',
+	'headings',
+	'rems',
+	'search-form',
+	'skip-links'
+) );
 
 // Add viewport meta tag for mobile browsers.
 add_theme_support( 'genesis-responsive-viewport' );
@@ -116,10 +123,13 @@ add_theme_support( 'genesis-after-entry-widget-area' );
 add_theme_support( 'genesis-footer-widgets', 3 );
 
 // Add Image Sizes.
-add_image_size( 'featured-image', 720, 400, TRUE );
+add_image_size( 'featured-image', 720, 400, true );
 
 // Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'pw-base' ), 'secondary' => __( 'Footer Menu', 'pw-base' ) ) );
+add_theme_support( 'genesis-menus', array(
+	'primary'   => __( 'After Header Menu', 'pw-base' ),
+	'secondary' => __( 'Footer Menu', 'pw-base' )
+) );
 
 // Reposition the secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
@@ -156,8 +166,6 @@ function pw_base_comments_gravatar( $args ) {
 }
 
 
-
-
 // Remove custom Genesis custom header style
 //remove_action( 'wp_head', 'genesis_custom_header_style' );
 /**********************************
@@ -165,13 +173,13 @@ function pw_base_comments_gravatar( $args ) {
  * Replace Header Site Title with Inline Logo
  *
  * @author AlphaBlossom / Tony Eppright, Neil Gee
- * @link http://www.alphablossom.com/a-better-wordpress-genesis-responsive-logo-header/
- * @link https://wpbeaches.com/adding-in-a-responsive-html-logoimage-header-via-the-customizer-for-genesis/
+ * @link   http://www.alphablossom.com/a-better-wordpress-genesis-responsive-logo-header/
+ * @link   https://wpbeaches.com/adding-in-a-responsive-html-logoimage-header-via-the-customizer-for-genesis/
  *
  * @edited by Sridhar Katakam
- * @link https://sridharkatakam.com/
+ * @link   https://sridharkatakam.com/
  *
-************************************/
+ ************************************/
 add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
 function custom_header_inline_logo( $title, $inside, $wrap ) {
 	if ( get_header_image() ) {
@@ -186,8 +194,10 @@ function custom_header_inline_logo( $title, $inside, $wrap ) {
 	$wrap = genesis_is_root_page() && ! genesis_get_seo_option( 'home_h1_on' ) ? 'h1' : $wrap;
 	// And finally, $wrap in h1 if HTML5 & semantic headings enabled
 	$wrap = genesis_html5() && genesis_get_seo_option( 'semantic_headings' ) ? 'h1' : $wrap;
+
 	return sprintf( '<%1$s %2$s>%3$s</%1$s>', $wrap, genesis_attr( 'site-title' ), $inside );
 }
+
 // Hide tagline
 // add_filter( 'genesis_attr_site-description', 'abte_add_site_description_class' );
 /**
@@ -195,24 +205,27 @@ function custom_header_inline_logo( $title, $inside, $wrap ) {
  *
  * Unhook this if you'd like to show the site description.
  *
+ * @param array $attributes Existing HTML attributes for site description element.
+ *
+ * @return string Amended HTML attributes for site description element.
  * @since 1.0.0
  *
- * @param array $attributes Existing HTML attributes for site description element.
- * @return string Amended HTML attributes for site description element.
  */
 function abte_add_site_description_class( $attributes ) {
 	$attributes['class'] .= ' screen-reader-text';
+
 	return $attributes;
 }
 
 //this function removes the "this topic contains..." and "this forum contains..."  text
-function no_description ($retstr) {
-$retstr="" ;
-return $retstr ;
+function no_description( $retstr ) {
+	$retstr = "";
+
+	return $retstr;
 }
 
-add_filter ('bbp_get_single_topic_description', 'no_description' ) ;
-add_filter ('bbp_get_single_forum_description', 'no_description' ) ;
+add_filter( 'bbp_get_single_topic_description', 'no_description' );
+add_filter( 'bbp_get_single_forum_description', 'no_description' );
 
 //This function changes the text wherever it is quoted
 // function change_translate_text( $translated_text ) {
@@ -233,8 +246,8 @@ function th_forum_breadcrumbs_root( $crumbs ) {
 
 	foreach ( $crumbs as $key => $crumb ) {
 
-		if( strpos( $crumb, 'bbp-breadcrumb-root' ) !== false ) {
-			unset( $crumbs[$key] );
+		if ( strpos( $crumb, 'bbp-breadcrumb-root' ) !== false ) {
+			unset( $crumbs[ $key ] );
 		}
 
 	}
@@ -242,6 +255,7 @@ function th_forum_breadcrumbs_root( $crumbs ) {
 	return $crumbs;
 
 }
+
 /*
 Plugin Name: bbPress - Custom Breadcrumbs
 Plugin URI: https://gist.github.com/ntwb/7781901
@@ -256,7 +270,8 @@ Author URI: http://netweb.com.au
 function ntwb_bbpress_custom_breadcrumb() {
 
 	// Forum root
-	$args['include_root']    = false;
+	$args['include_root'] = false;
+
 	//$args['root_text']    = 'text';
 
 	return $args;
@@ -265,9 +280,11 @@ function ntwb_bbpress_custom_breadcrumb() {
 // Increase number of forums
 
 function bbp_increase_forum_per_page( $args = array() ) {
-$args['posts_per_page'] = get_option( '_bbp_forums_per_page', 100 );
-return $args;
+	$args['posts_per_page'] = get_option( '_bbp_forums_per_page', 100 );
+
+	return $args;
 }
+
 add_filter( 'bbp_before_has_forums_parse_args', 'bbp_increase_forum_per_page' );
 
 /*
@@ -278,25 +295,56 @@ Version: 0.1
 Author: Stephen Edgar - Netweb
 Author URI: http://netweb.com.au
 */
-add_filter('protected_title_format', 'ntwb_remove_protected_title');
-function ntwb_remove_protected_title($title) {
+add_filter( 'protected_title_format', 'ntwb_remove_protected_title' );
+function ntwb_remove_protected_title( $title ) {
 	return '%s';
 }
-add_filter('private_title_format', 'ntwb_remove_private_title');
-function ntwb_remove_private_title($title) {
+
+add_filter( 'private_title_format', 'ntwb_remove_private_title' );
+function ntwb_remove_private_title( $title ) {
 	return '%s';
 }
+
 // remove post info.
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 
 //* Add new featured image size
-add_image_size( 'sm-thumb', 100, 100, TRUE );
+add_image_size( 'sm-thumb', 100, 100, true );
 
 // Register the three useful image sizes for use in Add Media modal
 add_filter( 'image_size_names_choose', 'amy_custom_sizes' );
 function amy_custom_sizes( $sizes ) {
-    return array_merge( $sizes, array(
-        'sm-thumb' => __( 'Smaller Thumbnail' ),
-    ) );
+	return array_merge( $sizes, array(
+		'sm-thumb' => __( 'Smaller Thumbnail' ),
+	) );
 }
+
+
+add_filter( 'genesis_markup_entry_open', 'th_add_anchors_to_blog' );
+/**
+ * Adds html ids to blog page article listings.
+ *
+ * @param $entry    string  Opening markup
+ *
+ * @return mixed    string  (maybe) filtered markup
+ * @since   1.0.0
+ *
+ */
+function th_add_anchors_to_blog( $entry ) {
+
+	global $post;
+
+	if ( ! $entry || is_single() || $post->post_type != 'post' ) {
+
+		return $entry;
+
+	}
+
+	$article_with_id = '<article id="post-' . $post->post_name . '" ';
+
+	return str_replace( '<article ', $article_with_id, $entry );
+
+}
+
+
 
