1697d7facf38888c94d8ddd8985035bc6b8d1144
mudevi <mudevi@gmail.com>
2019-04-26 02:01:20 -0400

    bring back image sizes into drop-down

--- wp-content/themes/dts-theme/functions.php
+++ wp-content/themes/dts-theme/functions.php
@@ -293,10 +293,10 @@ remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 add_image_size( 'sm-thumb', 100, 100, TRUE );
 
 // Register the three useful image sizes for use in Add Media modal
-// add_filter( 'image_size_names_choose', 'amy_custom_sizes' );
-// function amy_custom_sizes( $sizes ) {
-//     return array_merge( $sizes, array(
-//         'sm-thumb' => __( 'Smaller Thumbnail' ),
-//     ) );
-// }
+add_filter( 'image_size_names_choose', 'amy_custom_sizes' );
+function amy_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'sm-thumb' => __( 'Smaller Thumbnail' ),
+    ) );
+}
 
