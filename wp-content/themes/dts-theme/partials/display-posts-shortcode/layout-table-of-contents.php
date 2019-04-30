4fb1e233ca6ad93e737599b0a744ac00d9fb8633
Tomas Hartl <lachtani@gmail.com>
2019-04-30 13:28:22 -0400

    make table of contents links absolute

--- wp-content/themes/dts-theme/partials/display-posts-shortcode/layout-table-of-contents.php
+++ wp-content/themes/dts-theme/partials/display-posts-shortcode/layout-table-of-contents.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * "Small" layout for Display Posts Shortcode
+ * "Table of Contents" layout for Display Posts Shortcode
  *
  * @package
  * @author       Bill Erickson
@@ -9,9 +9,11 @@
  **/
 
 
-$post_listing_anchor = '#post-' . $post->post_name;
+$post_listing_anchor = '/guide/#post-' . $post->post_name;
+
+$url = esc_url( home_url( $post_listing_anchor ) );
 
 
 echo '<li class="listing-item">';
-echo '<a class="title" href="' . $post_listing_anchor . '">' . get_the_title() . '</a>';
+echo '<a class="title" href="' . $url . '">' . get_the_title() . '</a>';
 echo '</li>';
