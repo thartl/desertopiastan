402b8442233283a611e987e89ef8266cb5755252
Tomas Hartl <lachtani@gmail.com>
2019-05-02 15:17:46 -0400

    add a class to a button

---
+++ wp-content/plugins/core-functionality-dts/inc/loop-helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description
+ *
+ * @package     ${NAMESPACE}
+ * @since       1.0.0
+ * @author      thartl
+ * @link        https://parkdalewire.com
+ * @license     GNU General Public License 2.0+
+ */
